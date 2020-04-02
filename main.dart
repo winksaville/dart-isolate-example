e230f5c7e646a7cef16ef30895cdb70f6b24e3c8
Wink Saville <wink@saville.com>
2020-04-02 16:13:19 -0700

    Add some bidirectional communication.

--- main.dart
+++ main.dart
@@ -5,19 +5,55 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:isolate';
 
+// **********************************************
+// Added bidirectional communication, but this is
+// really ugly. Because start is a function that
+// creates an isolate and we can't listen on a
+// ReceivePort twice make the repsone port global.
+// Obviously, we should create a new object that has
+// a responsePort field. But this is simplest for
+// now and the "correct" way looks to be using an
+// IsolateChannel [1] and here is an example [2].
+//
+// [1](https://api.flutter.dev/flutter/package-stream_channel_isolate_channel/IsolateChannel-class.html)
+// [2](https://medium.com/@codinghive.dev/async-coding-with-dart-isolates-b09c5ec00f8b)
+// **********************************************
+
+SendPort responsePort = null;
+
 // Start an isolate and return it
 Future<Isolate> start() async {
   // Create a port used to communite with the isolate
-  ReceivePort receivePort= ReceivePort();
+  ReceivePort receivePort = ReceivePort();
 
   // Spawn runTimer in an isolate passing the sendPort so
   // it can send us messages
   Isolate isolate = await Isolate.spawn(runTimer, receivePort.sendPort);
 
+  // The first message on the receive port will be
+  // the sendPort that we can issue our responses to runTime
+  //   This doesn't work because wen listen here and
+  //   then again below, which leads to:
+  //      Unhanded exception:
+  //      Bad state: Stream has already been listened to.
+  // So we can't listen twice. Instead we have to make the
+  // responsePort "global" and in the listener function we
+  // use "if (data is SendPort)" to initialize the responsePort
+  //SendPort responsePort = await receivePort.first;
+  //stdout.writeln('Got the responsePort');
+
+
   // Listen on the receive port passing a routine that accepts
   // the data and prints it.
-  receivePort.listen((data) {
-    stdout.writeln('RECEIVE: ' + data);
+  receivePort.listen((dynamic data) {
+    if (data is SendPort) {
+      stdout.writeln('RECEIVE: responsePort');
+      responsePort = data;
+    } else {
+      assert(responsePort != null);
+      responsePort.send('RESPONSE: ' + data);
+      stdout.writeln('RECEIVE: ' + data);
+    }
   });
 
   // Return the isolate that was created
@@ -26,13 +62,23 @@ Future<Isolate> start() async {
 
 /// runTimer execptes to be in an isolate
 void runTimer(SendPort sendPort) {
+  // Send the "responsePort" to our partner
+  ReceivePort receivePort = ReceivePort();
+  sendPort.send(receivePort.sendPort);
+
   int counter = 0;
   Timer.periodic(new Duration(seconds: 1), (Timer t) {
     counter++;
     String msg = 'notification ' + counter.toString();  
-    stdout.write('SEND: ' + msg + ' - ');  
+    stdout.writeln('SEND: ' + msg);
     sendPort.send(msg);
   });
+
+  receivePort.listen((data) {
+    stdout.writeln('RESP: ' + data);
+  });
+
+  stdout.writeln('runTimer: done');
 }
 
 /// Stop the isolate immediately and return null
